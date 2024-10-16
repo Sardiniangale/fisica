34cb5c91bcb9b4d694cb70d90a38fa66b3d81f94
Coccicciala <tommasocicca06@gmail.com>
2024-10-16 15:44:48 +0200

    prova

---
+++ Coccicciala/tmp 2.py
@@ -0,0 +1,17 @@
+i=0
+#while i<7:
+ #   print(1)
+  #  i+=1
+
+while i<8:
+    print(i)
+    i+=1
+
+for i in range(10):
+    if i%2==0:
+        print(i)
+    else:
+        print (f' not {i} but 69')
+
+while i<420:
+    print (126)
