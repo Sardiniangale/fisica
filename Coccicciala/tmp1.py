b74f76866b7d31c4e54f02cad154fff336376c5d
Coccicciala <tommasocicca06@gmail.com>
2024-10-17 11:49:27 +0200

    boh

--- Coccicciala/tmp1.py
+++ Coccicciala/tmp1.py
@@ -16,6 +16,15 @@ z = statistics.mode(l)
 print("the mode is: ")
 print(z)
 
+#prove su come funziona il for dato che non l'ho capito
+for i in range(12):
+    print(f'{i+2.} free lunches at Conad')
+    print(f'{i*2} paid lunches at Conad') #i due print li fa alternati per il loop?
+
+#per quanto riguarda quello scritto qua sotto non mi ricordo dove volessi arrivare ma di sicuro non ha funzionato
+p='no thanks'
+for p in range(3):
+    print(p)
 
 
 
