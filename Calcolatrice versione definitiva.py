e65f936561546fbc4dcd68e10b8373a71a2bb892
Coccicciala <tommasocicca06@gmail.com>
2024-10-18 14:15:35 +0200

    Calcolatrice definitiva

--- Calcolatrice versione definitiva.py
+++ Calcolatrice versione definitiva.py
@@ -4,13 +4,11 @@ print('CALCOLATORE DI NUMERI PARI E DISPARI FINO AL NUMERO DESIDERATO')
 
 print('ATTENZIONE: in caso si risponda yes, evitare numeri numeri troppo alti')
 
-answer=input('Scrivere yes (o si) se si desidera calcolare tutti i numeri, no se il numero d'"'interesse è solo l'"'ultimo: ')
+a=input('Scrivere yes (o si) se si desidera calcolare tutti i numeri, no se il numero d'"'interesse è solo l'"'ultimo: ')
 
 n=int(input('Numero: ')) #metti fino a che numero vuoi controllare
 
-print('CALCOLATORE DI NUMERI PARI E DISPARI FINO AL NUMERO DESIDERATO')
-
-if answer.lower()=='yes' or 'si':
+if a.lower() == 'yes':
 
     for i in range(n//2+1): #n divisione intera per 2 per inserire nella lista i numeri pari solo fino al desiderato
         l.append(i*2)
@@ -21,16 +19,17 @@ if answer.lower()=='yes' or 'si':
         else:
             print(f'{i} is odd')
 
-elif answer.lower()=='conad': #easter egg per verificare quanti elif si possono mettere
-    print('Conad is love, Conad is life')
 
-elif answer.lower()=='no':
-    print('Per gentil cortesia rispondi solo con yes o no')
+if a.lower()=='no':
 
     if n//2!=(n-1)//2:
         print(f'{n} is even')
     else:
         print(f'{n} is odd')
 
-else: #in caso uno non risponda a modo
+elif a.lower()=='conad': #easter egg per verificare quanti elif si possono mettere
+    print('Conad is love, Conad is life')
+
+else:
     print('Per gentil cortesia rispondi solo con yes o no')
+
