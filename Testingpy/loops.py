8031e07b0e1e194fd99e0af7e865f0608e981b48
Sardiniangale <128549272+Sardiniangale@users.noreply.github.com>
2024-10-19 15:43:52 +0200

    A few things
    -Finished and compiled vector calculator, it is now a exe and no longer requires python to be run.
    
    -Organised folders

--- Testingpy/loops.py
+++ Testingpy/loops.py
@@ -1,41 +1,30 @@
-'''
-Created on 16 Oct 2024
-
-@author: Giacomo
-'''
 import numpy as np
 
-
-loops = int(input("Amount of vectors needed?: "))
-
 def vector_addition_r3():
+    num_vectors = int(input("Quanti vettori vuoi sommare? "))
+
+    vectors = []
+    for i in range(num_vectors):
+        
+        #the f is used for formating, otherwise it will break. 
+        
+        
+        print(f"Cosa sono le coordinate del {i+1}-esimo vettore (x, y, z):")
+        x = float(input("x: "))
+        y = float(input("y: "))
+        z = float(input("z: "))
+        vectors.append(np.array([x, y, z]))
+
+    result = np.sum(vectors, axis=0)
+    print("La somma dei vettori è:", result)
 
 
 
-lis = []
+#No idea what this is needed for
 
-for x in range(loops):
-    
-    x1 = float(input("x: "))
-    y1 = float(input("y: "))
-    z1 = float(input("z: "))
-    
-    x2 = float(input("x: "))
-    y2 = float(input("y: "))
-    z2 = float(input("z: "))
-    
-    v1 = np.array([x1, y1, z1])
-    v2 = np.array([x2, y2, z2])
-  
-  b = v1 + v2 
-  
-  
-  
-  
-print("La somma dei vettori e:", b)
-
-
-
-#I stole this, no idea why its needed
 if __name__ == "__main__":
     vector_addition_r3()
+    
+    
+    #Vector calculator completed, ill compile it now
+    
