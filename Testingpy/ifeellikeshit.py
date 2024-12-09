f9b3e8a94c447f61b5c34afcda98dfe64060f54c
Coccicciala <tommasocicca06@gmail.com>
2024-12-09 17:36:40 +0100

    Update ifeellikeshit.py

--- Testingpy/ifeellikeshit.py
+++ Testingpy/ifeellikeshit.py
@@ -1,20 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+n=int(input("Enter the precision required for the graph: "))
+
 def plot_function():
     print("Enter a mathematical function of x to plot, use regular numpy notation:")
     func_input = input("f(x) = ")
-    
+
     try:
 #hellaplugger
-        x = np.linspace(-10, 10, 500)
-        
+        x = np.linspace(-10, 10, n)
+
 #Ceirtified shitter
-      
+
         safe_env = {"np": np, "x": x}
-        
+
 #mannellas breath smells :0
-       
+
         y = eval(func_input, {"__builtins__": {}}, safe_env)
         plt.figure(figsize=(8, 6))
         plt.plot(x, y, label=f"f(x) = {func_input}")
@@ -26,10 +28,10 @@ def plot_function():
         plt.grid(color = 'gray', linestyle = '--', linewidth = 0.5)
         plt.legend()
         plt.show()
-    
+
     except Exception as e:
         print(f"An error occurred, respectfully learn basic maths.")
-        
+
 
 # Call the function
 plot_function()
